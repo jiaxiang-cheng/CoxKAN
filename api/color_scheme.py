95d471e994a0b2d455a93d2031da3ae12c22d975
Jia-Xiang Cheng <jiaxiang.cheng@outlook.com>
2024-07-09 12:46:34 +0800

    add the color scheme for visualization

---
+++ api/color_scheme.py
@@ -0,0 +1,13 @@
+AAAI_pal = {
+    "Dark Blue": "#061B35",
+    "Bright Blue": "#1240F5",
+    "Yellow":"#F0B546",
+    "Light Blue": "#E1EEFE",
+    "Main Blue": "#13396F"
+}
+
+box_pal = {
+    "25%": AAAI_pal['Bright Blue'],
+    "50%": AAAI_pal['Yellow'],
+    "75%": AAAI_pal['Main Blue']
+}
